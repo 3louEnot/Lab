215535f785281bb4324bc2a16cb6524db5c59e6a
3louEnot <haqe111q@gmail.com>
2022-11-21 13:31:48 +0200

    lab 3(5,6,7)

---
+++ 3-8.py
@@ -0,0 +1,6 @@
+num = int(input("Liczba: "))
+num2 = int(input("Liczba_2: "))
+for a in range(0, num2):
+	for b in range(0, num):
+		print("*", end = " ")
+	print("")
---
+++ 4 lab/4-1.py
@@ -0,0 +1,7 @@
+x = ['apple', 'banana', 'water', 'pizza']
+print(x[0])
+print(x[1])
+print(x)
+x[2] = 'mleko'
+print(x)
+print(x[-2:])
